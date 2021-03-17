10a730d6d23e58499e53cfe3aca7f96f9dabcb8f
gudangsoft <66896122+Gudangsoft@users.noreply.github.com>
2021-03-17 13:08:30 +0700

    Add files via upload

---
+++ multi_user/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Login Multi User Level Dengan PHP dan MySQLi </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 
+	<h1>Membuat Login Multi User Level Dengan PHP dan MySQLi <br/> </h1>
+
+	<?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan']=="gagal"){
+			echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+		}
+	}
+	?>
+ 
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+ 
+		<form action="cek_login.php" method="post">
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username .." required>
+ 
+			<label>Password</label>
+			<input type="password" name="password" class="form_login" placeholder="Password .." required>
+ 
+			<input type="submit" class="tombol_login" value="LOGIN">
+ 
+			<br/>
+			<br/>
+			
+		</form>
+		
+	</div>
+ 
+ 
+</body>
+</html>
